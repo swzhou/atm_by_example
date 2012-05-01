6adafb1189fbb36c558c8fc37a08859df180d3d6
swzhou <swing.zhou@hotmail.com>
2012-05-01 09:28:41 +0800

    add fake implementation of Account to pass first step definition

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -9,3 +9,11 @@ end
 Then /^\$(\d+) should be dispensed$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
+
+class Account
+
+  def initialize amount
+
+  end
+
+end
