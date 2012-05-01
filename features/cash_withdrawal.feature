7c2b96f6e864a2b62483448e6dc74c45b03eec2e
swzhou <swing.zhou@hotmail.com>
2012-05-01 12:36:22 +0800

    add step to check account balance after withdraw

--- features/cash_withdrawal.feature
+++ features/cash_withdrawal.feature
@@ -3,3 +3,4 @@ Feature: Cash withdrawal
     Given I have deposited $100 in my account
     When I withdraw $20
     Then $20 should be dispensed
+    And the balance of my account should be $80
--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -3,3 +3,7 @@ Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
   my_account.balance.should eq(amount),
                             "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
+
+Then /^the balance of my account should be (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  pending
+end
