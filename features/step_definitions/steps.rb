8cd3c5f0a8e3635be73142c773a3f95cc81dfd94
swzhou <swing.zhou@hotmail.com>
2012-05-01 09:20:57 +0800

    first failed step: have deposited money in my account

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Given /^I have deposited \$(\d+) in my account$/ do |amount|
+  Account.new(amount.to_i)
+end
+
+When /^I request \$(\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^\$(\d+) should be dispensed$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
