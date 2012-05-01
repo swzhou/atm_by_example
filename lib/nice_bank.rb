35e56f7df8e65217f289c1aff4ef75c8c8828412
swzhou <swing.zhou@hotmail.com>
2012-05-01 13:47:23 +0800

    withdraw page

--- lib/nice_bank.rb
+++ lib/nice_bank.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
 
 get '/' do
-  "Welcome to our nice bank."
+  %{
+<html>
+  <body>
+    <form action="/withdraw" method="post">
+      <label for="amount">Amount</label>
+      <input type="text" id="amount" name="amount"/>
+      <button type="submit">Withdraw</button>
+    </form>
+  </body>
+</html>
+  }
+end
+
+post '/withdraw' do
+  fail "I don't know how to withdraw yet, sorry"
 end
 
 class Account
