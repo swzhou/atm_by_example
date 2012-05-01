375d6354e6fe688e64d3494bc9741633be258590
swzhou <swing.zhou@hotmail.com>
2012-05-01 18:42:30 +0800

    add transaction queue and balance store for async process

--- lib/nice_bank.rb
+++ lib/nice_bank.rb
@@ -1,17 +1,23 @@
-require 'sinatra'
+require_relative 'transaction_queue'
+require_relative 'balance_store'
 
 class Account
 
+  def initialize
+    @queue = TransactionQueue.new
+    @balance_store = BalanceStore.new
+  end
+
   def credit(amount)
-    @balance = amount
+    @queue.write("+#{amount}")
   end
 
   def debit(amount)
-    @balance -= amount
+    @queue.write("-#{amount}")
   end
 
   def balance
-    @balance
+    @balance_store.balance
   end
 
 end
@@ -39,6 +45,8 @@ class CashSlot
     @amount = amount
   end
 end
+
+require 'sinatra'
 
 get '/' do
   %{
