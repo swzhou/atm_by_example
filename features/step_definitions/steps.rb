464d30ca7c8197166b15b5a0fe3a846b81159169
swzhou <swing.zhou@hotmail.com>
2012-05-01 09:56:24 +0800

    add verification after deposit 00

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,6 +1,7 @@
 Given /^I have deposited \$(\d+) in my account$/ do |amount|
   my_account = Account.new
   my_account.deposit(amount.to_i)
+  my_account.balance.should eq(amount.to_i)
 end
 
 When /^I request \$(\d+)$/ do |arg1|
@@ -14,6 +15,10 @@ end
 class Account
 
   def deposit(amount)
+
+  end
+
+  def balance
 
   end
 
